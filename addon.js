958176e92bce2dfe1314b684fadc660269cca924
USR <mozgbrasil@gmail.com>
2020-05-21 16:53:39 -0300

    commit - marcio - marcio-Inspiron-5548 - 21/05/2020 16:53:39

--- addon.js
+++ addon.js
@@ -7,119 +7,117 @@ const { addonBuilder } = require('stremio-addon-sdk');
 const fs = require('fs');
 var _ = require('lodash'); // Load the full build.
 
-log('cwd: ', process.cwd())
-log('__filename: ', __filename)
+//log('cwd: ', process.cwd())
+//log('__filename: ', __filename)
 
 var pathFile = `${__dirname}/metas.json`;
 var json = fs.readFileSync(pathFile, 'utf8');
 var metas = JSON.parse(json); // object
-
-log('metas[0]: ', metas[0])
-
+//log('metas[0]: ', metas[0])
 var sizeMetas = _.keys(metas).length;
 
 // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
-  "id": "community.naazguu", // https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md#basic-information
-  "name": "Nazzguu",
-  "description": "Catalog with " + sizeMetas + " titles",
-  "version": "1.0.1",
-  "resources": [ "catalog", "stream", "meta", "subtitles" ], // https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md#filtering-properties
-  "types": [ "movie", "series", "channel", "tv" ],
-  "catalogs": [{ // https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md#content-catalogs
-    "type": "movie",
-    "id": "top",
-    "name": "IPTV Nazzguu",
-    "extra": [
-        {
-          "name": "genres",
-          "isRequired": false,
-          "options": ["Action", "Comedy", "Drama"]
-        },
-    ]
-  }],
-  "background": "https://4.bp.blogspot.com/-BwM8BKuVAb8/VPw4f1a1ZII/AAAAAAACFRg/hfI5AFX_0bU/s1600/60.jpg",
-  "logo": "https://s.gravatar.com/avatar/e92be4ca866fbbb65e2e9a41512689fbs?s=256",
-  "contactEmail": "naazguu@gmail.com"
+    "id": "community.mozg", // https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md#basic-information
+    "name": "MOZG",
+    "description": "Catalog with " + sizeMetas + " titles",
+    "version": "1.0.1",
+    "resources": ["catalog", "stream", "meta", "subtitles"], // https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md#filtering-properties
+    "types": ["movie", "series", "channel", "tv"],
+    "catalogs": [{ // https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md#content-catalogs
+        "type": "movie",
+        "id": "top",
+        "name": "IPTV mozg",
+        "extra": [
+            {
+                "name": "genres",
+                "isRequired": false,
+                "options": ["Action", "Comedy", "Drama"]
+            },
+        ]
+    }],
+    "background": "https://4.bp.blogspot.com/-BwM8BKuVAb8/VPw4f1a1ZII/AAAAAAACFRg/hfI5AFX_0bU/s1600/60.jpg",
+    "logo": "https://s.gravatar.com/avatar/e92be4ca866fbbb65e2e9a41512689fbs?s=256",
+    "contactEmail": "mozgbrasil@gmail.com"
 }
 
 const builder = new addonBuilder(manifest)
 
-builder.defineCatalogHandler(({type, id, extra}) => {
-  log("request for catalogs: "+type+" "+id)
-  // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineCatalogHandler.md
-  const skip = parseInt(extra.skip || 0)
+builder.defineCatalogHandler(({ type, id, extra }) => {
+    log("request for catalogs: " + type + " " + id)
+    // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineCatalogHandler.md
+    const skip = parseInt(extra.skip || 0)
 
-  log('(typeof metas): ', (typeof metas));
-  log('skip: ', skip);
+    log('(typeof metas): ', (typeof metas));
+    log('skip: ', skip);
 
-  var metas_slice = metas.slice(skip, skip + 100)
-  //log('metas: ', metas);
-
-  var keys = _.keys(metas_slice);
-  log('keys: ', keys);
-
-  return Promise.resolve({ metas: metas_slice })
-})
-
-builder.defineMetaHandler(({type, id}) => {
-  log("request for meta: "+type+" "+id)
-  // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineMetaHandler.md
-  if ( id !== "") {
-  // serve one stream movie
+    var metas_slice = metas.slice(skip, skip + 100)
     //log('metas: ', metas);
-    var meta = _.find(metas, function(item) {
-      //log('item: ', item);
-      //log('id: ', id);
-      return item.id == id
-    })
-    //log('meta: ', meta);
 
-  return Promise.resolve({ meta: meta })
-  }
+    var keys = _.keys(metas_slice);
+    log('keys: ', keys);
 
-  // otherwise return no streams
-  return Promise.resolve({ meta: [] })
+    return Promise.resolve({ metas: metas_slice })
 })
 
-builder.defineStreamHandler(({type, id}) => {
-  log("request for streams: "+type+" "+id)
-  // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineStreamHandler.md
-  if ( id !== "") {
-  // serve one stream movie
-    //log('metas: ', metas);
-    var meta = _.find(metas, function(item) {
-      //log('item: ', item);
-      //log('id: ', id);
-      return item.id == id
-    })
-    //log('meta: ', meta);
+builder.defineMetaHandler(({ type, id }) => {
+    log("request for meta: " + type + " " + id)
+    // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineMetaHandler.md
+    if (id !== "") {
+        // serve one stream movie
+        //log('metas: ', metas);
+        var meta = _.find(metas, function (item) {
+            //log('item: ', item);
+            //log('id: ', id);
+            return item.id == id
+        })
+        //log('meta: ', meta);
 
-    const videos = meta.videos;
-    //log('videos: ', videos);
+        return Promise.resolve({ meta: meta })
+    }
 
-    const streams = [];
-    videos.forEach(function(item){
-      var _stream = item.stream;
-      streams.push(_stream);
-    });
-
-    //const streams = [{ infoHash: '8433e3c6435d6f8bbe5e65b21535dd2ff5ff91ac' }]
-    //const streams = [{ title: 'ss', url: 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4' }]
-
-    log('streams: ', streams);
-
-  return Promise.resolve({ streams: streams })
-  }
-
-  // otherwise return no streams
-  return Promise.resolve({ streams: [] })
+    // otherwise return no streams
+    return Promise.resolve({ meta: [] })
 })
 
-builder.defineSubtitlesHandler(({type, id}) => {
-  log("request for subtitles: "+type+" "+id)
-  // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineSubtitlesHandler.md
-  return Promise.resolve({ subtitles: [] })
+builder.defineStreamHandler(({ type, id }) => {
+    log("request for streams: " + type + " " + id)
+    // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineStreamHandler.md
+    if (id !== "") {
+        // serve one stream movie
+        //log('metas: ', metas);
+        var meta = _.find(metas, function (item) {
+            //log('item: ', item);
+            //log('id: ', id);
+            return item.id == id
+        })
+        //log('meta: ', meta);
+
+        const videos = meta.videos;
+        //log('videos: ', videos);
+
+        const streams = [];
+        videos.forEach(function (item) {
+            var _stream = item.stream;
+            streams.push(_stream);
+        });
+
+        //const streams = [{ infoHash: '8433e3c6435d6f8bbe5e65b21535dd2ff5ff91ac' }]
+        //const streams = [{ title: 'ss', url: 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4' }]
+
+        log('streams: ', streams);
+
+        return Promise.resolve({ streams: streams })
+    }
+
+    // otherwise return no streams
+    return Promise.resolve({ streams: [] })
+})
+
+builder.defineSubtitlesHandler(({ type, id }) => {
+    log("request for subtitles: " + type + " " + id)
+    // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineSubtitlesHandler.md
+    return Promise.resolve({ subtitles: [] })
 })
 
 module.exports = builder.getInterface()
--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 #!/usr/bin/env node
 
-const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
-const addonInterface = require("./addon")
-var port = (process.env.PORT || '7000'); // 64502
-serveHTTP(addonInterface, { port: port })
+const { addonBuilder, serveHTTP, getRouter, publishToCentral } = require("stremio-addon-sdk")
+const addonInterface = require("./addon");
+//const routerStremio = getRouter(addonInterface);
+//console.log('routerStremio: ', routerStremio);
+//process.argv.forEach((val, index) => {
+  //console.log(`process.argv: ${index}: ${val}`);
+//});
+var path = require('path');
+//const open = require('open');
+
+// Start the server
+(async () => {
+  var port = 3004;
+  const response = await serveHTTP(addonInterface, { port: port })
+  //console.log('response: ', response);
+  return response
+})()
+  .then(res => {
+    //console.log('res: ', res);
+    var url_stremio_manifest = res.url;
+    var url_stremio_landing =  url_stremio_manifest.replace('manifest.json', '');
+
+    (async () => {
+        // Opens the url in the default browser
+
+        var rootDir = path.dirname(__filename).split(path.sep).pop();
+        console.log(`🔶 🚀 Server App (${rootDir}) listening at %s`, url_stremio_landing);
+
+        //await open(url_stremio_manifest);
+        //await open(url_stremio_landing);
+    })();
+
+    //res.send(url_stremio_manifest);
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 // when you've deployed your addon, un-comment this line
-publishToCentral("https://stremio-dublados.herokuapp.com/manifest.json")
+//publishToCentral("https://stremio-dublados.herokuapp.com/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying.md
